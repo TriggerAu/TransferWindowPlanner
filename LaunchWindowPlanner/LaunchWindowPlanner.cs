82477606a9f07fa6a90afe64765f759893aa6a9b
TriggerAu <dtregoning@outlook.com.au>
2014-08-08 18:02:30 +1000

    Initial Project Creation

---
+++ LaunchWindowPlanner/LaunchWindowPlanner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using KSP;
+using UnityEngine;
+
+namespace LaunchWindowPlanner
+{
+    public class LaunchWindowPlanner
+    {
+
+    }
+}
