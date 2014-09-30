68d555034e7060ee41e70a441d5d9363391d6a22
TriggerAu <dtregoning@outlook.com.au>
2014-09-30 22:44:18 +1000

    WIP-KAC 3.0 API work (relates to #16)

--- TransferWindowPlanner/TWPWindowDebug.cs
+++ TransferWindowPlanner/TWPWindowDebug.cs
@@ -7,6 +7,8 @@ using KSP;
 using UnityEngine;
 using KSPPluginFramework;
 
+using TWP_KACWrapper;
+
 namespace TransferWindowPlanner
 {
     //public static class OrbitExtensions
@@ -89,17 +91,30 @@ namespace TransferWindowPlanner
 
                 //if (GUILayout.Button("KSP")) SkinsLibrary.SetCurrent(SkinsLibrary.DefSkinType.KSP);
                 //if (GUILayout.Button("UnityDef")) SkinsLibrary.SetCurrent(SkinsLibrary.DefSkinType.Unity);
-                if (GUILayout.Button("Default")) SkinsLibrary.SetCurrent("Default");
-                if (GUILayout.Button("Unity")) SkinsLibrary.SetCurrent("Unity");
-                if (GUILayout.Button("UnityWKSPButtons")) SkinsLibrary.SetCurrent("UnityWKSPButtons");
+                //if (GUILayout.Button("Default")) SkinsLibrary.SetCurrent("Default");
+                //if (GUILayout.Button("Unity")) SkinsLibrary.SetCurrent("Unity");
+                //if (GUILayout.Button("UnityWKSPButtons")) SkinsLibrary.SetCurrent("UnityWKSPButtons");
 
-                DrawLabel("MouseOverAny:{0}", mbTWP.MouseOverAnyWindow);
-                DrawLabel("MouseOverMain:{0}", mbTWP.windowMain.IsMouseOver);
-                DrawLabel("MouseOverMain:{0}", mbTWP.windowMain.IsMouseOver);
+                if (GUILayout.Button("CreateAlarm"))
+                {
+                    String tmp = KACWrapper.KAC.CreateAlarm(KACWrapper.KACAPI.AlarmTypeEnum.Raw,"Hello There",Planetarium.GetUniversalTime()+900);
+                    //tmp.Notes = "FRED FLINTSTONE";
 
-                DrawLabel("Settings:{0}", mbTWP.windowSettings.WindowRect);
-                DrawLabel("SettingsBlocker:{0}", mbTWP.windowSettingsBlockout.WindowRect);
-                DrawLabel("CI:{0} P:{1}", mbTWP.windowMain.ColorIndex, mbTWP.windowMain.Percent);
+                    KACWrapper.KAC.Alarms[0].Notes = "BARNEY RUBBLE";
+                }
+                DrawLabel("{0}", KACWrapper.KAC.Alarms.Count);
+                foreach ( KACWrapper.KACAPI.KACAlarm a in KACWrapper.KAC.Alarms)
+                {
+                    DrawLabel("{1}-{2}-{3} ({0})", a.ID,a.Name, a.Notes,a.AlarmType);
+                }
+
+                //DrawLabel("MouseOverAny:{0}", mbTWP.MouseOverAnyWindow);
+                //DrawLabel("MouseOverMain:{0}", mbTWP.windowMain.IsMouseOver);
+                //DrawLabel("MouseOverMain:{0}", mbTWP.windowMain.IsMouseOver);
+
+                //DrawLabel("Settings:{0}", mbTWP.windowSettings.WindowRect);
+                //DrawLabel("SettingsBlocker:{0}", mbTWP.windowSettingsBlockout.WindowRect);
+                //DrawLabel("CI:{0} P:{1}", mbTWP.windowMain.ColorIndex, mbTWP.windowMain.Percent);
 
                 DrawLabel("Mouse:{0}", mbTWP.windowMain.vectMouse);
                 DrawLabel("Plot:{0}", new Rect(mbTWP.windowMain.PlotPosition.x, mbTWP.windowMain.PlotPosition.y, mbTWP.windowMain.PlotWidth, mbTWP.windowMain.PlotHeight));
